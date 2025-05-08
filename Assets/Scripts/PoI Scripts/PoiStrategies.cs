174dd92d43946df4e25a8effc90d2d0ba5874a92
Woopsious <reecehand@hotmail.co.uk>
2025-05-08 23:26:10 +0100

    undone changes from PoiStrategies as best i could. still missing a part of prototype code i had done before i wiped it clean.

--- Assets/Scripts/PoI Scripts/PoiStrategies.cs
+++ Assets/Scripts/PoI Scripts/PoiStrategies.cs
@@ -1,18 +1,91 @@
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.AI;
 
 public interface IPoIStrategies
 {
+	public void Start()
+	{
 
+	}
+
+	public void Update(float deltaTime)
+	{
+
+	}
+
+	public void Stop()
+	{
+
+	}
 }
 
 public class PoiCapture : IPoIStrategies
 {
+	readonly PoIController poIController;
+	readonly PoiData _Data;
 
+	CountdownTimer timer;
+	bool captureInProgress;
+
+	public PoiCapture(PoIController poIController, float duration)
+	{
+		this.poIController = poIController;
+		poIController._PoiData = _Data;
+		timer = new CountdownTimer(duration);
+		timer.OnTimerStart += () => StartCapture();
+		timer.OnTimerStop += () => FinishCapture();
+	}
+
+	public void Start()
+	{
+		timer.Start();
+	}
+
+	public void Update(float deltaTime)
+	{
+		timer.Tick(deltaTime, false);
+	}
+
+	public void StartCapture()
+	{
+		captureInProgress = true;
+	}
+	public void CancelCapture()
+	{
+		captureInProgress = false;
+	}
+
+	public void FinishCapture()
+	{
+		captureInProgress = false;
+	}
 }
 
 public class PoiAddResources : IPoIStrategies
 {
+	CountdownTimer timer;
 
+	public PoiAddResources(float duration)
+	{
+		timer = new CountdownTimer(duration);
+		timer.OnTimerStart += () => AddResources();
+		timer.OnTimerStop += () => AddResources();
+	}
+
+	public void Start()
+	{
+		timer.Start();
+	}
+
+	public void Update(float deltaTime)
+	{
+		timer.Tick(deltaTime, false);
+	}
+
+	public void AddResources()
+	{
+
+	}
 }
